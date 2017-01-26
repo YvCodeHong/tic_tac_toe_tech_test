3d458c62958161d57a4ac6b914282ca6c6e86655
Asuka Ochi <asukaochi@gmail.com>
2017-01-26 10:45:53 +0000

    refactor add_player method

--- lib/game.rb
+++ lib/game.rb
@@ -3,8 +3,18 @@ class Game
   attr_accessor :player1, :player2
 
   def add_player( player )
-    raise "Already two players exist" if !player2.nil?
-    player1.nil? ? self.player1 = player : self.player2 = player
+    raise "Already two players exist" if player2_exist?
+    player1_exist? ? self.player2 = player : self.player1 = player
+  end
+
+  private
+
+  def player1_exist?
+    !player1.nil?
+  end
+
+  def player2_exist?
+    !player2.nil?
   end
 
 end
