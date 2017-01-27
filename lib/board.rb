75ec9b07d3ae8d533d8fce6085ac232e47b5a54e
Asuka Ochi <asukaochi@gmail.com>
2017-01-27 09:00:39 +0000

    modify Board class

--- lib/board.rb
+++ lib/board.rb
@@ -3,13 +3,14 @@ class Board
   attr_accessor :all_fields
   attr_reader :size
 
-  def initialize
-    self.all_fields = []
-  end
-
-  def create(size)
+  def initialize(size)
     raise "Set size more than 3" if size < 3
     @size = size
+    @all_fields = []
+    create
+  end
+
+  def create
     row = []
     size.times{ row << false }
     size.times{ self.all_fields << row.dup }
